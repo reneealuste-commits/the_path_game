a32492ac55f4dabaf7aa59aa012985e46f9dd386
Vibecode Terminal <terminal@vibecodeapp.com>
2026-01-02 23:50:15 +0000

    Sandbox autosave

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app">
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useGameStore } from './stores/game'
+
+const gameStore = useGameStore()
+
+onMounted(() => {
+  gameStore.initialize()
+})
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
