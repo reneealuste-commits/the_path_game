383419eb752440c31c3a3a59a55454220a3f0c74
Vibecode Terminal <terminal@vibecodeapp.com>
2026-01-02 23:49:56 +0000

    Sandbox autosave

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import restart from 'vite-plugin-restart'
+
+export default defineConfig({
+  root: './',
+  publicDir: './public/',
+  base: './',
+  server: {
+    host: true,
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env),
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  plugins: [
+    vue(),
+    restart({ restart: ['public/**'] })
+  ]
+})
